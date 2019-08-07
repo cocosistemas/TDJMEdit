26ff22fd517401363f1f082e17abdc1736165017
Diego Jose Muñoz Carbajo <diego@cocosistemas.com>
2019-08-07 12:25:46 +0200

    proyecto test Berlin

--- TestBerlin/TestDJMEditBerlin.dpr
+++ TestBerlin/TestDJMEditBerlin.dpr
@@ -1,12 +1,15 @@
 program TestDJMEditBerlin;
 
 uses
-  Vcl.Forms;
+  Vcl.Forms,
+  fTestDJMEdit in 'fTestDJMEdit.pas' {frTestDJMEdit},
+  uTDJMEdit in '..\uTDJMEdit.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrTestDJMEdit, frTestDJMEdit);
   Application.Run;
 end.
